bb7e79dff19ad231418478fe51ef732722bdc46a
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-05-25 16:04:31 -0300

    feat : Projeto Backend Criado

---
+++ PetHouseBackend/src/main/java/io/GitHub/AugustoMello09/PetHouseBackend/PetHouseBackendApplication.java
@@ -0,0 +1,13 @@
+package io.GitHub.AugustoMello09.PetHouseBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHouseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHouseBackendApplication.class, args);
+	}
+
+}
---
+++ PetHouseBackend/src/test/java/io/GitHub/AugustoMello09/PetHouseBackend/PetHouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.GitHub.AugustoMello09.PetHouseBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
