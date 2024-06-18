1266d13849c880d2a32a2eba8e891445966d1d24
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-06-18 15:40:15 -0300

    test : Add/remover produto do carrinho Service/Controller

--- PetHouseBackend/src/test/java/io/GitHub/AugustoMello09/PetHouseBackend/controllers/CarrinhoControllerTeste.java
+++ PetHouseBackend/src/test/java/io/GitHub/AugustoMello09/PetHouseBackend/controllers/CarrinhoControllerTeste.java
@@ -2,6 +2,7 @@ package io.GitHub.AugustoMello09.PetHouseBackend.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.util.UUID;
@@ -24,6 +25,8 @@ import io.GitHub.AugustoMello09.PetHouseBackend.services.CarrinhoService;
 public class CarrinhoControllerTeste {
 	
 	private static final UUID ID = UUID.fromString("148cf4fc-b379-4e25-8bf4-f73feb06befa");
+	
+	private static final Long IDPRODUTO = 1L;
 	
 	@InjectMocks
 	private CarrinhoController controller;
@@ -51,6 +54,27 @@ public class CarrinhoControllerTeste {
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertEquals(ResponseEntity.class, response.getClass());
 		assertEquals(CarrinhoDTO.class, response.getBody().getClass());
+	}
+	
+	@DisplayName("Deve adicionar um Produto ao carrinho. ")
+	@Test
+	public void ShouldAddProtudoOnCarrinho() {
+		CarrinhoDTO carrinhoDTO = carrinhoDTOProvider.criar();
+		doNothing().when(service).adicionarAoCarrinho(carrinhoDTO);
+		ResponseEntity<Void> response = controller.adicionarProdutos(carrinhoDTO);
+		assertNotNull(response);
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+	
+	@DisplayName("Deve remover um Produto do carrinho. ")
+	@Test
+	public void ShouldRemoveItemFromCarrinho() {
+		doNothing().when(service).removerProdutoDoCarrinho(ID, IDPRODUTO);
+		ResponseEntity<Void> response = controller.removerProduto(ID, IDPRODUTO);
+		assertNotNull(response);
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
 	}
 
 }
--- PetHouseBackend/src/test/java/io/GitHub/AugustoMello09/PetHouseBackend/services/CarrinhoServiceTeste.java
+++ PetHouseBackend/src/test/java/io/GitHub/AugustoMello09/PetHouseBackend/services/CarrinhoServiceTeste.java
@@ -1,12 +1,16 @@
 package io.GitHub.AugustoMello09.PetHouseBackend.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,8 +25,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import io.GitHub.AugustoMello09.PetHouseBackend.dtos.CarrinhoDTO;
 import io.GitHub.AugustoMello09.PetHouseBackend.entities.Carrinho;
+import io.GitHub.AugustoMello09.PetHouseBackend.entities.Produto;
 import io.GitHub.AugustoMello09.PetHouseBackend.provider.CarrinhoDTOProvider;
 import io.GitHub.AugustoMello09.PetHouseBackend.provider.CarrinhoProvider;
+import io.GitHub.AugustoMello09.PetHouseBackend.provider.ProdutoProvider;
 import io.GitHub.AugustoMello09.PetHouseBackend.repotories.CarrinhoRepository;
 import io.GitHub.AugustoMello09.PetHouseBackend.repotories.ProdutoRepository;
 import io.GitHub.AugustoMello09.PetHouseBackend.services.exceptions.ObjectNotFoundException;
@@ -31,6 +37,8 @@ import io.GitHub.AugustoMello09.PetHouseBackend.services.exceptions.ObjectNotFou
 public class CarrinhoServiceTeste {
 
 	private static final UUID ID = UUID.fromString("148cf4fc-b379-4e25-8bf4-f73feb06befa");
+	
+	private static final Long IDPRODUTO = 1L;
 
 	@InjectMocks
 	private CarrinhoServiceImpl service;
@@ -40,12 +48,13 @@ public class CarrinhoServiceTeste {
 
 	@Mock
 	private ModelMapper mapper;
-	
+
 	@Mock
 	private ProdutoRepository produtoRepository;
 
 	private CarrinhoProvider carrinhoProvider;
 	private CarrinhoDTOProvider carrinhoDTOPrivider;
+	private ProdutoProvider produtoProvider;
 
 	@BeforeEach
 	public void setUp() {
@@ -53,6 +62,7 @@ public class CarrinhoServiceTeste {
 		service = new CarrinhoServiceImpl(repository, mapper, produtoRepository);
 		carrinhoDTOPrivider = new CarrinhoDTOProvider();
 		carrinhoProvider = new CarrinhoProvider();
+		produtoProvider = new ProdutoProvider();
 	}
 
 	@DisplayName("Deve retornar um carrinho")
@@ -70,11 +80,86 @@ public class CarrinhoServiceTeste {
 	@Test
 	public void shouldReturnCarrinhoNotFound() {
 		when(repository.findById(any(UUID.class))).thenReturn(Optional.empty());
-
 		ObjectNotFoundException exception = assertThrows(ObjectNotFoundException.class, () -> {
 			service.findById(any(UUID.class));
 		});
 		assertEquals("Carrinho não encontrado", exception.getMessage());
+	}
+
+	@DisplayName("Deve retornar carrinho não encontrado")
+	@Test
+	public void shouldReturnCarrinhoNotFoundWhenAddProducts() {
+		CarrinhoDTO carrinhoDTO = carrinhoDTOPrivider.criar();
+		when(repository.findById(any(UUID.class))).thenReturn(Optional.empty());
+		ObjectNotFoundException exception = assertThrows(ObjectNotFoundException.class, () -> {
+			service.adicionarAoCarrinho(carrinhoDTO);
+		});
+		assertEquals("Carrinho não encontrado", exception.getMessage());
+	}
+
+	@DisplayName("Deve conseguir adicionar um produto ao carrinho")
+	@Test
+	public void shouldAddProtudoOnCarrinhoDTO() {
+		Carrinho carrinho = carrinhoProvider.criar();
+		CarrinhoDTO carrinhoDTO = carrinhoDTOPrivider.criar();
+		Produto produto = produtoProvider.criar();
+		List<Produto> produtosParaAdicionar = List.of(produto);
+
+		when(repository.findById(any(UUID.class))).thenReturn(Optional.of(carrinho));
+		when(produtoRepository.findAllById(anyList())).thenReturn(produtosParaAdicionar);
+		when(repository.save(any(Carrinho.class))).thenReturn(carrinho);
+		
+		service.adicionarAoCarrinho(carrinhoDTO);
+		
+		assertEquals(1, carrinho.getProdutos().size());  
+	    assertTrue(carrinho.getProdutos().contains(produto));  
+
+	    verify(repository).findById(any(UUID.class));  
+	    verify(produtoRepository).findAllById(anyList()); 
+	    verify(repository).save(any(Carrinho.class)); 
+	}
+	
+	@DisplayName("Deve retornar carrinho não encontrado ao tentar remover")
+	@Test
+	public void RemoveItemToCarrinhoShouldReturnCarrinhoNotFound() {
+		when(repository.findById(any(UUID.class))).thenReturn(Optional.empty());
+		ObjectNotFoundException exception = assertThrows(ObjectNotFoundException.class, () -> {
+			service.removerProdutoDoCarrinho(ID, IDPRODUTO);
+		});
+		assertEquals("Carrinho não encontrado", exception.getMessage());
+	}
+	
+	@DisplayName("Deve retornar produto não encontrado ao tentar remover")
+	@Test
+	public void RemoveItemToCarrinhoShouldReturnProdutoNotFound() {
+		Carrinho carrinho = carrinhoProvider.criar();
+		when(repository.findById(any(UUID.class))).thenReturn(Optional.of(carrinho));
+		when(produtoRepository.findById(IDPRODUTO)).thenReturn(Optional.empty());
+		ObjectNotFoundException exception = assertThrows(ObjectNotFoundException.class, () -> {
+			service.removerProdutoDoCarrinho(ID, IDPRODUTO);
+		});
+		assertEquals("Produto não encontrado", exception.getMessage());
+	}
+	
+	@DisplayName("Deve remover produto do carrinho com sucesso")
+	@Test
+	public void ShouldRemoveItemFromCarrinhoWithSuccess() {
+		Carrinho carrinho = carrinhoProvider.criar();
+		Produto produto = produtoProvider.criar();
+		carrinho.getProdutos().add(produto);
+		
+		when(repository.findById(any(UUID.class))).thenReturn(Optional.of(carrinho));
+		when(produtoRepository.findById(IDPRODUTO)).thenReturn(Optional.of(produto));
+		when(repository.save(any(Carrinho.class))).thenReturn(carrinho);
+		
+		service.removerProdutoDoCarrinho(carrinho.getId(), produto.getId());
+		
+		assertFalse(carrinho.getProdutos().contains(produto));  
+	    assertEquals(0, carrinho.getProdutos().size());  
+
+	    verify(repository).findById(any(UUID.class));  
+	    verify(produtoRepository).findById(IDPRODUTO);  
+	    verify(repository).save(any(Carrinho.class));
 	}
 
 }
