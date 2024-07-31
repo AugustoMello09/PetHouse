81ba9d8ad68a53d9a7e8ec8ad919d02c036ccf2f
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-07-31 14:36:22 -0300

    feat : Domain Entities/enum

---
+++ PatHouseBackend/PetHouse/src/main/java/io/gitHub/AugustoMello09/PetHouse/domain/entities/Pedido.java
@@ -0,0 +1,68 @@
+package io.gitHub.AugustoMello09.PetHouse.domain.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Setter
+@Getter
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	
+	@Column(nullable = false)
+	private LocalDate data;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name = "tb_pedido_produto", joinColumns = @JoinColumn(name = "pedido_id"), inverseJoinColumns = @JoinColumn(name = "produto_id"))
+	private List<Produto> produtos = new ArrayList<>();
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_id", referencedColumnName = "id")
+	private Usuario usuario;
+	
+	@ManyToOne
+	@JoinColumn(name = "carrinho_id", referencedColumnName = "id")
+	private Carrinho carrinho;
+
+	public Pedido(UUID id, LocalDate data, Usuario usuario) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.usuario = usuario;
+	}
+	
+	public BigDecimal getValorTotalPedido() {
+		return produtos.stream()
+				.map(Produto::getPreco)
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+	}
+
+}
---
+++ PatHouseBackend/PetHouse/src/main/java/io/gitHub/AugustoMello09/PetHouse/domain/enums/Plano.java
@@ -0,0 +1,27 @@
+package io.gitHub.AugustoMello09.PetHouse.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Plano {
+	
+	BASICO(0, "Básico"), ESSENCIAL(1, "Essencial"), COMPLETO(2, "Completo");
+	
+	private Integer cod;
+	private String descricao;
+
+	public static Plano toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+
+		for (Plano x : Plano.values()) {
+			if (cod.equals(x.cod)) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Cod invalido" + cod);
+	}
+}
