bee2d5f9ca270b5d6bc5821863050a378b2108d8
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-07-31 15:04:59 -0300

    feat : CargoService/CargoController

---
+++ PatHouseBackend/PetHouse/src/main/java/io/gitHub/AugustoMello09/PetHouse/domain/dtos/CargoDTO.java
@@ -0,0 +1,18 @@
+package io.gitHub.AugustoMello09.PetHouse.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CargoDTO {
+
+	private Long id;
+
+	private String authority;
+
+}
---
+++ PatHouseBackend/PetHouse/src/main/java/io/gitHub/AugustoMello09/PetHouse/services/serviceImpl/CargoServiceImpl.java
@@ -0,0 +1,62 @@
+package io.gitHub.AugustoMello09.PetHouse.services.serviceImpl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import io.gitHub.AugustoMello09.PetHouse.domain.dtos.CargoDTO;
+import io.gitHub.AugustoMello09.PetHouse.domain.entities.Cargo;
+import io.gitHub.AugustoMello09.PetHouse.repositories.CargoRepository;
+import io.gitHub.AugustoMello09.PetHouse.services.CargoService;
+import io.gitHub.AugustoMello09.PetHouse.services.exceptions.ObjectNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class CargoServiceImpl implements CargoService {
+
+	private final CargoRepository repository;
+
+	private final ModelMapper mapper;
+
+	@Override
+	public CargoDTO findById(Long id) {
+		Optional<Cargo> entity = repository.findById(id);
+		Cargo cargo = entity.orElseThrow(() -> new ObjectNotFoundException("Cargo não encontrado"));
+		return mapper.map(cargo, CargoDTO.class);
+	}
+
+	@Override
+	public List<CargoDTO> listAll() {
+		List<Cargo> cargos = repository.findAll();
+		return cargos.stream().map(x -> mapper.map(x, CargoDTO.class)).collect(Collectors.toList());
+	}
+
+	@Override
+	public CargoDTO create(CargoDTO cargoDTO) {
+		Cargo cargo = new Cargo();
+		cargo.setAuthority(cargoDTO.getAuthority());
+		repository.save(cargo);
+		return mapper.map(cargo, CargoDTO.class);
+	}
+
+	@Override
+	public void updateCargo(CargoDTO cargoDTO, Long id) {
+		Cargo cargo	= repository.findById(id)
+				.orElseThrow(() -> new ObjectNotFoundException("Cargo não encontrado"));
+		cargo.setAuthority(cargoDTO.getAuthority());
+		repository.save(cargo);
+		mapper.map(cargo, CargoDTO.class);
+
+	}
+
+	@Override
+	public void deleteCargo(Long id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+
+}
