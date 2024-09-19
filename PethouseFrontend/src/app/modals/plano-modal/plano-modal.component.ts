33837808616b51202fe9c899f74d7ce93fa9c880
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-09-18 21:52:28 -0300

    feat : Modal Plano

--- PethouseFrontend/src/app/modals/plano-modal/plano-modal.component.ts
+++ PethouseFrontend/src/app/modals/plano-modal/plano-modal.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { jwtDecode } from 'jwt-decode';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Plano } from 'src/app/model/plano.model';
+import { AuthService } from 'src/app/service/auth.service';
+import { PlanoService } from 'src/app/service/plano.service';
+import { AuthModalComponent } from '../auth-modal/auth-modal.component';
 
 @Component({
   selector: 'app-plano-modal',
@@ -7,9 +15,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanoModalComponent implements OnInit {
 
-  constructor() { }
+  plano!: Plano;
+
+  constructor(private service: PlanoService,
+    public dialogRef: MatDialogRef<PlanoModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { id: number }, private auth: AuthService, private dialog: MatDialog,
+    private snack: MatSnackBar, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.findById(this.data.id);
+  }
+
+  findById(id: number) {
+    this.service.findById(id).subscribe(plano => {
+      this.plano = plano;
+    });
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  public comprarPlano() {
+    this.spinner.show();
+    const isAuthenticated = this.auth.isAuthenticated();
+    if (!isAuthenticated) {
+      this.dialog.open(AuthModalComponent);
+    } else {
+      const token = this.auth.obterToken();
+      if (token) {
+        const decodedToken: any = jwtDecode(token);
+        const userId = decodedToken.id; 
+        this.service.comprarPlano(this.data.id, userId).subscribe(() => {
+          this.spinner.hide();
+          this.message("Plano comprado com sucesso!");
+        });
+      }
+    }
+  }
+
+  public message(msg: string) {
+    this.snack
+      .open(`${msg}`, 'OK', {
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        duration: 8000
+    })
   }
 
 }
