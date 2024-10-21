7ea259cffce8bb37f32b0e975a87c272e7b322cd
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-10-21 13:06:45 -0300

    Docker-compose funcionando

--- PetHouseBackend/PetHouse/src/test/java/io/gitHub/AugustoMello09/PetHouse/PetHouseApplicationTests.java
+++ PetHouseBackend/PetHouse/src/test/java/io/gitHub/AugustoMello09/PetHouse/PetHouseApplicationTests.java
@@ -1,10 +1,9 @@
 package io.gitHub.AugustoMello09.PetHouse;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class PetHouseApplicationTests {
 
 	@Test
